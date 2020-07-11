542b2581d925825f5d2893ab650f5972bb8f9412
JenkinsZhang <614758656@qq.com>
2020-07-11 15:34:18 +0800

    Add Gateway's cors configuration

--- gateway/src/main/java/com/jenkins/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/jenkins/gateway/GatewayApplication.java
@@ -5,7 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
  * @author JenkinsZhang
@@ -22,5 +28,23 @@ public class GatewayApplication {
         Environment env = app.run(args).getEnvironment();
         LOG.info("Starting Successfully!");
         LOG.info("Eureka address: http://localhost:{}",env.getProperty("server.port"));
+    }
+
+    /**
+     * Cors Configuration
+     */
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(Boolean.TRUE);
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**",corsConfiguration);
+
+        return new CorsWebFilter(source);
     }
 }
--- server/src/main/java/com/jenkins/server/config/CorsConfig.java
+++ server/src/main/java/com/jenkins/server/config/CorsConfig.java
@@ -1,25 +1,25 @@
-package com.jenkins.server.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * @author JenkinsZhang
- * @date 2020/7/11
- */
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true)
-                .maxAge(3600);
-
-    }
-}
+//package com.jenkins.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * @author JenkinsZhang
+// * @date 2020/7/11
+// */
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .allowCredentials(true)
+//                .maxAge(3600);
+//
+//    }
+//}
